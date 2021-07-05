1662852735465030d477b0e1f39f76b5a56cce4c
Nick Pattison <pattison.nick@gmail.com>
2021-07-05 15:55:55 -0400

    Modified GridCell and GridController

--- Assets/Scripts/GridCell.cs
+++ Assets/Scripts/GridCell.cs
@@ -1,18 +1,74 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class GridCell : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+public enum Direction {
+    RightDown,
+    Down,
+    LeftDown,
+    LeftUp,
+    Up,
+    RightUp
 }
+
+
+public class GridCell : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public int cellType { get; private set; }
+    public int xIndex { get; set; }
+    public int yIndex { get; set; }
+
+    public Color[] colors;
+
+    private void Start() {
+        cellType = Random.Range(0, colors.Length);
+        Image img = GetComponent<Image>();
+        if(img != null) {
+            img.color = colors[cellType];
+        }
+    }
+    public void Init() {
+        Text txt = GetComponentInChildren<Text>();
+        if(txt != null) {
+            txt.text = xIndex + ", " + yIndex;
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData) {
+        
+        //print("My value is: " + cellType);
+    }
+
+    public void OnPointerUp(PointerEventData eventData) {
+
+        Vector2 cellPos = new Vector2(transform.position.x, transform.position.y);
+        Vector2 vectorToMouse = eventData.position - cellPos;
+        
+        if(vectorToMouse.magnitude > 30) {
+            float radians = Mathf.Atan2(vectorToMouse.y, vectorToMouse.x);
+            float degrees = radians * Mathf.Rad2Deg;
+
+            if (degrees >= 0 && degrees < 60) GridController.grid.TrySwap(this, Direction.RightUp);
+            if (degrees >= 60 && degrees < 120) GridController.grid.TrySwap(this, Direction.Up);
+            if (degrees >= 120 && degrees < 180) GridController.grid.TrySwap(this, Direction.LeftUp);
+            if (degrees >= -180 && degrees < -120) GridController.grid.TrySwap(this, Direction.LeftDown);
+            if (degrees >= -120 && degrees < -60) GridController.grid.TrySwap(this, Direction.Down);
+            if (degrees >= -60 && degrees < 0) GridController.grid.TrySwap(this, Direction.RightDown);
+
+        }
+    } // ends OnPointerUp()
+
+    public void PositionCell() {
+
+        Vector2 pos = new Vector2(xIndex, yIndex) * GridController.grid.gridSpacing;
+        if (xIndex % 2 == 0) { // even columns
+            pos.y += GridController.grid.gridSpacing / 2; // move up
+        }
+
+        RectTransform rt = (RectTransform)transform;
+        rt.anchoredPosition = pos;
+    }
+
+} // ends GridCell
--- Assets/Scripts/GridController.cs
+++ Assets/Scripts/GridController.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class GridController : MonoBehaviour {
 
+    static public GridController grid; // singleton
+
+    public float gridSpacing = 51;
     public int gridWidth = 4;
     public int gridHeight = 4;
 
@@ -12,6 +16,8 @@ public class GridController : MonoBehaviour {
     GridCell[,] cells;
 
     private void Start() {
+        GridController.grid = this;
+
         BuildGrid(gridWidth, gridHeight);
     }
 
@@ -26,12 +32,85 @@ public class GridController : MonoBehaviour {
             for(int y = 0; y < h; y++) {
 
                 GridCell cell = Instantiate(cellPrefab, transform);
-                RectTransform rt = (RectTransform) cell.transform;
-                Vector2 pos = new Vector2(x, y) * 51;
-                rt.anchoredPosition = pos;
+
+                cell.xIndex = x;
+                cell.yIndex = y;
+
+                cell.PositionCell();
+
+                cell.Init();
+
+                cells[x, y] = cell;
 
             } // ends y loop
         } // ends x loop
 
     } // ends BuildGrid()
+
+    public void TrySwap(GridCell cell, Direction dir) {
+        //print("swap " + dir);
+
+        GridCell cell2 = FindNeighborCell(cell, dir);
+
+        if (cell2 == null) return; // no neighboring cell in that direction, do nothing...
+
+        // TODO: swap the two cells
+
+        // swap position in array:
+        cells[cell.xIndex, cell.yIndex] = cell2;
+        cells[cell2.xIndex, cell2.yIndex] = cell;
+
+        int tempX = cell.xIndex;
+        int tempY = cell.yIndex;
+
+        cell.xIndex = cell2.xIndex;
+        cell.yIndex = cell2.yIndex;
+
+        cell2.xIndex = tempX;
+        cell2.yIndex = tempY;
+
+        cell.PositionCell();
+        cell2.PositionCell();
+
+        // TODO: check for matches
+
+        // TODO: if no matches, unswap
+
+    }
+   
+
+    private GridCell FindNeighborCell(GridCell cell, Direction dir) {
+
+        if (cell == null) return null;
+
+        if (dir == Direction.Up) return GetCellAt(cell.xIndex, cell.yIndex + 1);
+        if (dir == Direction.Down) return GetCellAt(cell.xIndex, cell.yIndex - 1);
+
+        if (cell.xIndex % 2 == 0) { // even column
+
+            if (dir == Direction.LeftUp) return GetCellAt(cell.xIndex - 1, cell.yIndex + 1);
+            if (dir == Direction.LeftDown) return GetCellAt(cell.xIndex - 1, cell.yIndex);
+            if (dir == Direction.RightUp) return GetCellAt(cell.xIndex + 1, cell.yIndex + 1);
+            if (dir == Direction.RightDown) return GetCellAt(cell.xIndex + 1, cell.yIndex);
+
+        } else { // odd column
+
+            if (dir == Direction.LeftUp) return GetCellAt(cell.xIndex - 1, cell.yIndex);
+            if (dir == Direction.LeftDown) return GetCellAt(cell.xIndex - 1, cell.yIndex - 1);
+            if (dir == Direction.RightUp) return GetCellAt(cell.xIndex + 1, cell.yIndex);
+            if (dir == Direction.RightDown) return GetCellAt(cell.xIndex + 1, cell.yIndex - 1);
+        }
+        return null;
+    }
+
+    public GridCell GetCellAt(int x, int y) {
+
+        if (x < 0) return null;
+        if (y < 0) return null;
+        if (x >= cells.GetLength(0)) return null;
+        if (y >= cells.GetLength(1)) return null;
+
+        return cells[x, y];
+    }
+
 } // ends GridController
