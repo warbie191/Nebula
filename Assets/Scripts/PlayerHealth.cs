0dda5221af1ef922bd0b53425522a6d3698de755
Ryan <ryan.warblow@gmail.com>
2021-07-11 21:50:32 -0400

    Health and Shield system with logic to take damage
    Added code to remove health and restore health.
    Used the same logic to enable damage to sheilds so shields must be depletted prior to damaging hull or health. Also put a function into restore health will probably do that for shields as well.

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,112 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private float health;
+    private float shields;
+    private float lerpTimer;
+    public float maxHealth = 100;
+    public float maxShields = 100;
+    public float chipSpeed = 2f;
+    public Image frontHealthBar;
+    public Image backHealthBar;
+    public Image frontShieldBar;
+    public Image backShieldBar;
+
+    
+    
+    void Start()
+    {
+        health = maxHealth;
+        shields = maxShields;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        health = Mathf.Clamp(health, 0, maxHealth);
+        shields = Mathf.Clamp(shields, 0, maxShields);
+        UpdateSheildUI();
+        UpdateHealthUI();
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            TakeDamage(Random.Range(5, 10));
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            RestoreHealth(Random.Range(5, 10));
+        }
+    }
+
+    public void UpdateHealthUI()
+    {
+        Debug.Log("Health value at: " + health);
+        float fillF = frontHealthBar.fillAmount;
+        float fillB = backHealthBar.fillAmount;
+        float hFraction = health / maxHealth;
+        if(fillB > hFraction)
+        {
+            frontHealthBar.fillAmount = hFraction;
+            backHealthBar.color = Color.yellow;
+            lerpTimer += Time.deltaTime;
+            float percentComplete = lerpTimer / chipSpeed;
+            percentComplete = percentComplete * percentComplete; //for animation purpose makes transition smoother
+            backHealthBar.fillAmount = Mathf.Lerp(fillB, hFraction, percentComplete);
+        }
+        if(fillF < hFraction)
+        {
+            backHealthBar.color = Color.green;
+            backHealthBar.fillAmount = hFraction;
+            lerpTimer += Time.deltaTime;
+            float percentComplete = lerpTimer / chipSpeed;
+            percentComplete = percentComplete * percentComplete; //for animation purpose makes transition smoother
+            frontHealthBar.fillAmount = Mathf.Lerp(fillF, backHealthBar.fillAmount, percentComplete);
+        }
+
+    }
+
+    public void UpdateSheildUI()
+    {
+        Debug.Log("Shield power at: " + shields);
+        float fillF = frontShieldBar.fillAmount;
+        float fillB = backShieldBar.fillAmount;
+        float hFraction = shields / maxShields;
+        if (fillB > hFraction)
+        {
+            frontShieldBar.fillAmount = hFraction;
+            backShieldBar.color = Color.red;
+            lerpTimer += Time.deltaTime;
+            float percentComplete = lerpTimer / chipSpeed;
+            percentComplete = percentComplete * percentComplete; //for animation purpose makes transition smoother
+            backShieldBar.fillAmount = Mathf.Lerp(fillB, hFraction, percentComplete);
+        }
+
+    }
+
+    public void TakeDamage(float damage)
+    {
+        if (shields > 0)
+        {
+            shields -= damage;
+            lerpTimer = 0f;
+        }
+        else { 
+        health -= damage;
+        lerpTimer = 0f;
+        }
+    }
+
+    public void RestoreHealth(float healAmount)
+    {
+        health += healAmount;
+        lerpTimer = 0f;
+            
+     }
+
+
+}
